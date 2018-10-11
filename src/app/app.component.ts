0e295464fa936d7c73776f422b982c3f3fddb602
Karolina Mamczarz <karomamczi@gmail.com>
2018-10-11 23:57:49 +0200

    feat: Add items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { TodoItem } from './interfaces/todo-item';
       Welcome to {{ title }}!
     </h1>
 ​
-    <app-input-button-unit></app-input-button-unit>
+    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
 
     <ul>
       <li *ngFor="let todoItem of todoList">
@@ -28,4 +28,8 @@ export class AppComponent {
     { title: 'develop app' },
     { title: 'deploy app' }
   ];
+
+  addItem(title: string): void {
+    this.todoList.push({ title });
+  }
 }
--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <input #inputElementRef [value]="title" (keyup.enter)="changeTitle(inputElementRef.value)">
-    <button (click)="changeTitle(inputElementRef.value)">Save</button>
+    <input #inputElementRef [value]="title" (keyup.enter)="submitValue(inputElementRef.value)">
+    <button (click)="submitValue(inputElementRef.value)">Save</button>
   `,
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
+  @Output()
+  submit: EventEmitter<string> = new EventEmitter<string>();
   title = 'Hello World';
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  changeTitle(newTitle: string): void {
-    this.title = newTitle;
+  submitValue(newTitle: string): void {
+    this.submit.emit(newTitle);
   }
 }
