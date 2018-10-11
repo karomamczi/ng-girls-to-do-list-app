5e1de9d1c359d44c7860fad69df0a93a1be1d646
Karolina Mamczarz <karomamczi@gmail.com>
2018-10-11 23:11:36 +0200

    feat: The To Do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,23 @@ import { Component } from '@angular/core';
     </h1>
 ​
     <app-input-button-unit></app-input-button-unit>
+
+    <ul>
+      <li *ngFor="let todoItem of todoList">
+        {{ todoItem.title }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ng-girls-to-do-list-app';
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' }
+  ];
 }
